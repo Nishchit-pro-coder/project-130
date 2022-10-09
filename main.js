430b7ee2997984f3ba5aaeb5e24365e64e3566e5
nishchit pro coder <99675062+Nishchit-pro-coder@users.noreply.github.com>
2022-10-09 11:38:18 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,13 @@ song3 = "";
 song4 = "";
 song5 = "";
 
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+scoreLeftWrist = 0;
+scoreRightWrist = 0;
+
 function preload() 
 {
     song1 = loadSound("doraemone.mp3");
@@ -21,6 +28,8 @@ function setup()
     video = createCapture(VIDEO);
     video.hide();
 
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
 }
 
 function modelLoaded() 
@@ -34,6 +43,15 @@ function draw()
 
     fill("#FF0000");
     stroke("#FF0000")
+
+    if(scoreRightWrist > 0.2)
+    {
+        circle(rightWristX, rightWristY, 20);
+    }
+    if(scoreLeftWrist > 0.2)
+    {
+        circle(leftWristX, leftWristY, 20);
+    }
 }
 
 function stopall() 
@@ -78,4 +96,23 @@ function playBeliever()
     song5.play();
     song5.setVolume(1);
     song5.rate(1);
+}
+
+function gotPoses(results) 
+{
+    if(results.length > 0) 
+    {
+        console.log(results);
+        scoreRightWrist = results[0].pose.keypoints[10].score;
+        scoreLeftWrist = results[0].pose.keypoints[9].score;
+        console.log("scoreLeftWrist = " + scoreLeftWrist + "scoreRightWrist = " + scoreRightWrist);
+
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("leftWristX = " + leftWristX +"leftWristY = " + leftWristY);
+        
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX = " + rightWristX +"rightWristY = " + rightWristY);
+    }
 }
